30eca88d60087e2cc7e9339acbaa6fae1f167d81
Arian van Putten <arian.vanputten@gmail.com>
2025-01-10 12:30:17 +0100

    update vendorHash

--- nix/package.nix
+++ nix/package.nix
@@ -4,5 +4,5 @@ buildGoModule {
   pname = "cgroup-exporter";
   version = "0.1.0";
   src = ../.;
-  vendorHash = "sha256-srQcHjMVz9wV96eAX9P9iRtvi7CHqZC+GZSsh+gkrvU=";
+  vendorHash = "sha256-NnvB20rORPS5QF5enbb5KpWaKZ70ybSgfd7wjk21/Cg=";
 }
